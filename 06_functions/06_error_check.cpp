7389a06613188299e8f0186c92942f9957f33788
sushiramenboi <cerbitowarner2@gmail.com>
2026-08-17 19:10:40 -0700

    Add function example

---
+++ 06_functions/06_error_check.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void PrintDate(int currDay, int currMonth, int currYear)
+{
+
+    // Parameter error checking
+    if ((currDay < 1) || (currDay > 31))
+    {
+        cout << "Invalid day (" << currDay << "). Using 1." << endl;
+        currDay = 1;
+    }
+
+    if ((currMonth < 1) || (currMonth > 12))
+    {
+        cout << "Invalid month (" << currMonth << "). Using 1." << endl;
+        currMonth = 1;
+    }
+
+    // Begin function's normal behavior
+    cout << currMonth << "/" << currDay << "/" << currYear;
+}
+
+int main()
+{
+
+    PrintDate(30, 7, 2012);
+    cout << endl
+         << endl;
+
+    PrintDate(40, 7, 2012);
+    cout << endl
+         << endl;
+
+    PrintDate(30, 13, 2012);
+    cout << endl
+         << endl;
+
+    return 0;
+}
